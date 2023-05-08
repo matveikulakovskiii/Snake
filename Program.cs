b596770732bf4070ab185b267e383365476fdb3d
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2023-05-08 11:48:22 +0300

    Add files via upload
    создание змейки

--- Program.cs
+++ Program.cs
@@ -23,7 +23,8 @@ namespace Snake
             Rightline.Drow();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
         }
     }
 }
